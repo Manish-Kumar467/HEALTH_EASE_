f5df316cc1d4dcd78f3c13fa093003a1cdd1ceb6
Manish-Kumar467 <mkwork467@gmail.com>
2024-11-16 23:03:59 +0530

    changed the pickle to joblib

--- predict.py
+++ predict.py
@@ -1,5 +1,5 @@
 import sys
-import pickle
+from joblib import load
 import numpy as np
 import os 
 import sklearn
@@ -14,19 +14,27 @@ else:
     print(f"Model file found at {model_path}")
 
 try:
-    with open(model_path, "rb") as file:
-        model = pickle.load(file)
+    # with open(model_path, "rb") as file:
+    #     model = joblib.load(file)
+    model = load(model_path) # (Standard way) Automatically handles file opening and closing
     print("Model loaded successfully.")
+    print(f"Loaded object type: {type(model)}")
     # Check if the model has the predict method
     if not hasattr(model, 'predict'):
         raise ValueError("Loaded model does not have a 'predict' method.")
 except Exception as e:
-    print(f"Error loading model: {e}")
+    print(f"Error loading model=>: {e}")
     sys.exit(1)
 
 # Parse input from Node.js
-input_data = list(map(float, sys.argv[1:]))  # Accept comma-separated features # get the values from node js as sys.argv
-input_array = np.array(input_data).reshape(1, -1)
+try:
+    input_data = list(map(float, sys.argv[1:]))  # Accept comma-separated features # get the values from node js as sys.argv
+    input_array = np.array(input_data).reshape(1, -1)
+    print(f"Input data: {input_array}")
+except Exception as e:
+    print(f"Error parsing input data: {e}")
+    sys.exit(1)
+
 
 # Make prediction
 try:
